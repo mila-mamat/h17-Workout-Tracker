2a4f9b69e4ac734d9739ff9d16ead452139cf862
Mila <mila.windsor@gmail.com>
2020-06-06 20:56:12 -0400

    minor changes to the html page nav bar

--- server.js
+++ server.js
@@ -21,46 +21,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 
-// const campusLibrary = new db.Workout({'day':Date.now(),'exercises':[] })
-// const out = new db.Exercise({
-//   day: new Date().setDate(new Date().getDate()-10),
-
-//       type: "resistance",
-//       name: "Bicep Curl",
-//       duration: 30,
-//       weight: 90,
-//       reps: 10,
-//       sets: 4
-// })
-// out.save()
-
-//   const out2 = new db.Exercise({
-//     day: new Date().setDate(new Date().getDate()-10),
-
-//         type: "resistance",
-//         name: "Bicep Curl",
-//         duration: 20,
-//         weight: 100,
-//         reps: 10,
-//         sets: 4
-
-//   })
-//   out2.save()
-//   .then(async () => {
-
-//       campusLibrary.exercises.push(out._id, out2._id)
-//       campusLibrary
-//       .save()
-//     })
-
-//     campusLibrary
-//   .save()
-//   .then(dbLibrary => console.log(dbLibrary))
-//   .catch(({ message }) => console.log(message))
-
-
-
-
 // routes
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
